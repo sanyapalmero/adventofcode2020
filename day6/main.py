297b2cab8f196a696badddec21fc1d0bceb0affa
Alexandr Vagner <sanyapalmero@gmail.com>
2020-12-07 00:39:05 +0500

    Update read_file function

--- day6/main.py
+++ day6/main.py
@@ -4,22 +4,23 @@ from operator import __and__
 
 def read_file():
     with open("input.txt") as file:
-        return file.read().splitlines()
+        lines = file.read().splitlines()
+        lines.append(None)
+        return lines
 
 
 def part_1():
     yes_count = 0
-
     lines = read_file()
+    
     questions = set()
     for line in lines:
         if line:
-            for question in line:
-                questions.add(question)
+            questions.update(line)  
         else:
             yes_count += len(questions)
             questions = set()
-    yes_count += len(questions)
+
     print(yes_count)
 
 
@@ -34,7 +35,7 @@ def part_2():
         else:
             yes_count += len(list(reduce(__and__, questions)))
             questions = []
-    yes_count += len(list(reduce(__and__, questions)))
+
     print(yes_count)
 
 
